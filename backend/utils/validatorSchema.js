69cb2cda70f709c6d80fa666e1296b90f807faf2
Ayinla <sherifdeenraji96@gmail.com>
2025-04-19 00:52:19 +0100

    Refactor registration schema to use CommonJS module exports

--- backend/utils/validatorSchema.js
+++ backend/utils/validatorSchema.js
@@ -1,7 +1,11 @@
 const Joi = require("joi");
 
-export const registerSchema = Joi.object({
+const registerSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).max(100).required(),
-})
+});
+
+module.exports = {
+  registerSchema,
+};
